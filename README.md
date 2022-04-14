e1bc636b2750a1a55839a2ccff283ad76b962ac6
Maxim-Pekov <max.pekov@gmail.com>
2022-04-14 17:36:29 +0300

    add creditcalc.py v.5

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Loan-Calculator
-It takes several parameters like a loan principal and interest, calculates the values the user wants to know (for example, monthly payment or overpayment), and outputs them to the user.
+Implemented a loan calculator, through the command line.
+#Command example
+__**python creditcalc.py --principal=1000000 --periods=60 --interest=10 --type diff**__    
+`    -----_This command will help you find the differential payment on a loan.`_
+
+--type indicates the type of payment: "annuity" or "diff" (differentiated). If --type is specified neither as "annuity" nor as "diff" or not specified at all, you will see the error message.  
+--payment is the monthly payment amount.
+--principal is used for calculations of both types of payment. You can get its value if you know the interest, annuity payment, and number of months.
+--periods denotes the number of months needed to repay the loan. It's calculated based on the interest, annuity payment, and principal.
+--interest is specified without a percent sign. Note that it can accept a floating-point value. Our loan calculator can't calculate the interest, so it must always be provided.
--- creditcalc.py
+++ creditcalc.py
@@ -10,15 +10,16 @@ parser.add_argument("--periods", type=int)
 parser.add_argument("--interest", type=float)
 args = parser.parse_args()
 commands = [args.type, args.payment, args.principal, args.periods, args.interest]
-
+sum_payment = 0
 
 def dif(p, n, i, mo):
-    d = p / n + (i / 12 * 100) + (p - (p * (mo - 1) / n))
-    print(f"Month {mo}: payment is {round(d)}")
+    d = p / n + (i / 12 / 100) * (p - (p * (mo - 1) / n))
+    print(f"Month {mo}: payment is {math.ceil(d)}")
+    return math.ceil(d)
 
 
-def periods(lp, mp):
-    i = round(lp / 12 / 100)
+def periods(lp, mp, li):
+    i = li / 12 / 100
     n = math.ceil(math.log((mp / (mp - i * lp)), (1 + i)))
     return n
 
@@ -27,27 +28,61 @@ def annuity(lp, per, li):
     i = li / 12 / 100
     a = math.ceil(lp * (i * ((1 + i) ** per)) / (((1 + i) ** per) - 1))
     print(f"Your annuity payment = {a}!")
+    return a
 
 
 def prin(per, li, pay):
     i = li / 12 / 100
     p = pay / (i * math.pow((1 + i), per) / ((1 + i) ** per - 1))
+    print(f"Your loan principal = {math.floor(p)}!")
     return p
 
 if args.principal != "none" and args.periods != "none" and args.interest != "none" and args.type == "diff" and args.payment == None:
     month = 1
+    overpayment = 0
     while month != args.periods + 1:
-        dif(args.principal, args.periods, args.interest, month)
+        overpayment += dif(args.principal, args.periods, args.interest, month)
+        if month == args.periods:
+            print(f"Overpayment = {round(overpayment - args.principal)}")
         month += 1
-elif args.principal != "none" and args.periods != "none" and args.interest != "none" and args.type == "annuity" and args.payment == None:
-    annuity(args.principal, args.periods, args.interest)
 
-#periods(args.principal, args.payment)
-#annuity(args.principal, args.periods, args.interest)
-#prin(args.periods, args.interest, args.payment)
-print(args)
+
+elif args.principal != None and args.periods != None and args.interest != None and args.type == "annuity" and args.payment == None:
+    a = annuity(args.principal, args.periods, args.interest)
+    overpayment = args.periods * a - args.principal
+    print(f"Overpayment = {round(overpayment)}")
+
+
+elif args.payment != None and args.periods != None and args.interest != None and args.type == "annuity":
+    p = prin(args.periods, args.interest, args.payment)
+    overpayment = args.periods * args.payment - p
+    print(f"Overpayment = {math.ceil(overpayment)}")
+
+
+elif args.type == "annuity" and args.payment != None and args.interest != None and args.principal != None:
+    n = periods(args.principal, args.payment, args.interest)
+    overpayment = n * args.payment - args.principal
+    year_qlt = n // 12
+    month_qlt = n % 12
+    if year_qlt < 1:
+        if month_qlt <= 1:
+            print(f"It will take {month_qlt} month to repay this loan!")
+        else:
+            print(f"It will take {month_qlt} months to repay this loan!")
+    elif year_qlt >= 1:
+        if month_qlt == 0:
+            print(f"It will take {year_qlt} years to repay this loan!")
+        elif month_qlt == 1:
+            print(f"It will take {year_qlt} years and 1 month month to repay this loan!")
+        elif month_qlt > 1:
+            print(f"It will take {year_qlt} years and {month_qlt} months to repay this loan!")
+    print(f"Overpayment = {round(overpayment)}")
+
+
+else:
+    print("Incorrect parameters")
+
 
 if args.type != "diff" and args.type != "annuity":
-    print("Incorect parameters")
-else:
-    print("OK")
+    print("Incorrect parameters")
+
